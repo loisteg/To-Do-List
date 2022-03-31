78d5d33e0ddb1771aba413ff261f9c633174a49e
Loisteg <loisteg@outlook.com>
2022-03-31 14:38:55 +0300

    Added To do List, working w/ design

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,63 @@
-import './App.css';
+import {useState} from "react";
+import { Card } from 'react-bootstrap';
+import Todo from "./Todo";
+import FormTodo from "./FormTodo";
+import StartPage from "./StartPage"
+
+import "./App.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
 
+  const [todos, setTodos] = useState([
+    {
+      text: "Just example",
+      isDone: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const markTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <>
+      <div className="app">
+      <StartPage/>
+       {/* <div className="container">
+          <h1 className="text-center mb-4">Todo List</h1>
+          <FormTodo addTodo={addTodo} />
+          <div>
+            {todos.map((todo, index) => (
+              <Card>
+                <Card.Body>
+                  <Todo
+                  key={index}
+                  index={index}
+                  todo={todo}
+                  markTodo={markTodo}
+                  removeTodo={removeTodo}
+                  />
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
+      </div> */}
     </div>
+    </>
   );
 }
 
---
+++ todo/src/StartPage.js
@@ -0,0 +1,15 @@
+import './startPage.scss'
+
+
+const StartPage = () => {
+    return (
+        <div className='starting__modal'>
+            <h1>To Do List</h1>
+            <h2>My portfolio project</h2>
+
+            <h2 className='start'>Let's start</h2>
+        </div>
+    )
+}
+
+export default StartPage;
